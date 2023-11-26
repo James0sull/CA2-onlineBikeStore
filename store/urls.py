c36005e363bc7238aa34aa394fc9be802470acf3
James O'Sullivan <x00202782@mytudublin.ie>
2023-11-26 14:51:58 +0000

    Brand html updated + error displaying commit 13

--- store/urls.py
+++ store/urls.py
@@ -4,10 +4,11 @@ from . import views
 app_name = "store"
 
 urlpatterns = [
-    
     path('', views.bike_list, name="all_bikes"),
-    path('<uuid:brand_id>/', views.brand_list, name='bikes_by_brand'),
-    path('<uuid:brand_id>/<uuid:bike_id>/', views.bike_detail, name = 'bike_detail'),
-    path('<uuid:brand_id>/<uuid:bike_id>/<uuid:sale_id>/', views.sale_detail, name = 'sale_detail'),
-
+    path('all_brands/', views.all_brands, name="all_brands"),
+    path('<uuid:brand_id>/', views.brand_list, name='brands'),
+    path('brand/<uuid:brand_id>/', views.brand_detail, name='brand_detail'),
+    path('<uuid:brand_id>/<uuid:bike_id>/', views.bike_detail, name='bike_detail'),
+    path('<uuid:brand_id>/<uuid:bike_id>/<uuid:sale_id>/', views.sale_detail, name='sale_detail'),
 ]
+
