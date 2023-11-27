074d6f15997e9a031aad4a49909ff0104866296b
James0sull <145473944+James0sull@users.noreply.github.com>
2023-11-27 18:00:37 +0000

    Delete accounts directory
    causing Issue

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class CustomUser(AbstractUser):
-    email = models.CharField(max_length=250, null=True, blank=False)
-    
--- accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path 
-from store import views
-from .views import SignUpView
-
-urlpatterns = [
-    path('create/', SignUpView.as_view(), name='signup')
-]
