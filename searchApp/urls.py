8b5be179f44bfdda89fa3d49f646a9f4aa1a9379
James0sull <145473944+James0sull@users.noreply.github.com>
2023-11-28 11:16:08 +0000

    Delete searchApp directory

--- searchApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SearchResultsListView
-
-app_name='searchApp'
-
-urlpatterns = [
- path('', SearchResultsListView.as_view(), name='searchResult'),
-]
