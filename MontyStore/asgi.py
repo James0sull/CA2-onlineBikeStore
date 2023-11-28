bbcb059da75808ac8d563fb666441ad334a52905
James0sull <145473944+James0sull@users.noreply.github.com>
2023-11-28 10:50:53 +0000

    virtual evn issues fixed

--- MontyStore/asgi.py
+++ MontyStore/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MonthyStore project.
+ASGI config for MontyStore project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MonthyStore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MontyStore.settings')
 
 application = get_asgi_application()
--- MontyStore/urls.py
+++ MontyStore/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for MonthyStore project.
+URL configuration for MontyStore project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
