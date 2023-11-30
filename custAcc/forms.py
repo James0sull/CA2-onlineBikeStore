1f6ab5e8e6d1df6127393b6c0332d67037dafb23
James O'Sullivan <x00202782@mytudublin.ie>
2023-11-30 17:37:11 +0000

    create acc working but issue with login

--- custAcc/forms.py
+++ custAcc/forms.py
@@ -1,11 +1,31 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Address
 
 class CustomUserCreationForm(UserCreationForm):
+    street = forms.CharField(max_length=255)
+    city = forms.CharField(max_length=255)
+    county = forms.CharField(max_length=255)
+    post_code = forms.CharField(max_length=255)
+
     class Meta:
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('username', 'email')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.save()
+
+        # Create associated Address instance
+        Address.objects.create(
+            customer=user,
+            street=self.cleaned_data['street'],
+            city=self.cleaned_data['city'],
+            county=self.cleaned_data['county'],
+            post_code=self.cleaned_data['post_code']
+        )
+
+        return user
     
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
--- custAcc/views.py
+++ custAcc/views.py
@@ -1,24 +1,13 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.contrib.auth.models import Group
 from .forms import CustomUserCreationForm
-from .models import CustomUser
-
-# Create your views here.
+from django.urls import reverse_lazy
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
- 
-    def post(self, request, *args, **kwargs):
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            signup_user = CustomUser.objects.get(username=username)
-            customer_group = Group.objects.get(name='Customer')
-            customer_group.user_set.add(signup_user)
-            return redirect('login')
-        else:
-            return render(request, self.template_name, {'form' : form })
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Any additional logic you want to include after successful form submission
+        return response
